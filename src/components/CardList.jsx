bbc29c081048197db4e151f1f0d7cfaa98b76aeb
Mantas Samaitis <mantaksam@gmail.com>
2020-03-19 19:22:18 +0200

    first commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  console.log("CardList");
+  return (
+    <div>
+      {robots.map(robot => (
+        <Card
+          key={robot.id}
+          id={robot.id}
+          name={robot.name}
+          username={robot.username}
+          email={robot.email}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
